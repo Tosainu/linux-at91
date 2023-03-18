502b850b3dbb0c91817c3b8c9982ca197ccc2818
Kenta Sato <tosainu.maple@gmail.com>
2023-03-18 12:31:02 +0900

    Revert "wifi: wilc1000: fix DMA on stack objects"
    This reverts commit fc49ef4feab6398c536c8737a46aa92fb67596c6.

--- drivers/net/wireless/microchip/wilc1000/sdio.c
+++ drivers/net/wireless/microchip/wilc1000/sdio.c
@@ -40,7 +40,6 @@ struct wilc_sdio {
 	u32 block_size;
 	bool isinit;
 	struct wilc *wl;
-	u8 *cmd53_buf;
 };
 
 struct sdio_cmd52 {
@@ -60,7 +59,6 @@ struct sdio_cmd53 {
 	u32 count:		9;
 	u8 *buffer;
 	u32 block_size;
-	bool use_global_buf;
 };
 
 static const struct wilc_hif_func wilc_hif_sdio;
@@ -110,8 +108,6 @@ static int wilc_sdio_cmd53(struct wilc *wilc, struct sdio_cmd53 *cmd)
 {
 	struct sdio_func *func = container_of(wilc->dev, struct sdio_func, dev);
 	int size, ret;
-	struct wilc_sdio *sdio_priv = wilc->bus_data;
-	u8 *buf = cmd->buffer;
 
 	sdio_claim_host(func);
 
@@ -122,25 +118,12 @@ static int wilc_sdio_cmd53(struct wilc *wilc, struct sdio_cmd53 *cmd)
 	else
 		size = cmd->count;
 
-	if (cmd->use_global_buf) {
-		if (size > sizeof(u32))
-			return -EINVAL;
-
-		buf = sdio_priv->cmd53_buf;
-	}
-
 	if (cmd->read_write) {  /* write */
-		if (cmd->use_global_buf)
-			memcpy(buf, cmd->buffer, size);
-
 		ret = sdio_memcpy_toio(func, cmd->address,
-				       buf, size);
+				       (void *)cmd->buffer, size);
 	} else {        /* read */
-		ret = sdio_memcpy_fromio(func, buf,
+		ret = sdio_memcpy_fromio(func, (void *)cmd->buffer,
 					 cmd->address,  size);
-
-		if (cmd->use_global_buf)
-			memcpy(cmd->buffer, buf, size);
 	}
 
 	sdio_release_host(func);
@@ -164,12 +147,6 @@ static int wilc_sdio_probe(struct sdio_func *func,
 	sdio_priv = kzalloc(sizeof(*sdio_priv), GFP_KERNEL);
 	if (!sdio_priv)
 		return -ENOMEM;
-
-	sdio_priv->cmd53_buf = kzalloc(sizeof(u32), GFP_KERNEL);
-	if (!sdio_priv->cmd53_buf) {
-		ret = -ENOMEM;
-		goto free;
-	}
 
 	if (IS_ENABLED(CONFIG_WILC_HW_OOB_INTR))
 		io_type = WILC_HIF_SDIO_GPIO_IRQ;
@@ -233,7 +210,6 @@ dispose_irq:
 	irq_dispose_mapping(wilc->dev_irq_num);
 	wilc_netdev_cleanup(wilc);
 free:
-	kfree(sdio_priv->cmd53_buf);
 	kfree(sdio_priv);
 	return ret;
 }
@@ -245,7 +221,6 @@ static void wilc_sdio_remove(struct sdio_func *func)
 
 	clk_disable_unprepare(wilc->rtc_clk);
 	wilc_netdev_cleanup(wilc);
-	kfree(sdio_priv->cmd53_buf);
 	kfree(sdio_priv);
 	wilc_bt_deinit();
 }
@@ -468,9 +443,8 @@ static int wilc_sdio_write_reg(struct wilc *wilc, u32 addr, u32 data)
 		cmd.address = WILC_SDIO_FBR_DATA_REG;
 		cmd.block_mode = 0;
 		cmd.increment = 1;
-		cmd.count = sizeof(u32);
+		cmd.count = 4;
 		cmd.buffer = (u8 *)&data;
-		cmd.use_global_buf = true;
 		cmd.block_size = sdio_priv->block_size;
 		ret = wilc_sdio_cmd53(wilc, &cmd);
 		if (ret)
@@ -508,7 +482,6 @@ static int wilc_sdio_write(struct wilc *wilc, u32 addr, u8 *buf, u32 size)
 	nblk = size / block_size;
 	nleft = size % block_size;
 
-	cmd.use_global_buf = false;
 	if (nblk > 0) {
 		cmd.block_mode = 1;
 		cmd.increment = 1;
@@ -587,9 +560,8 @@ static int wilc_sdio_read_reg(struct wilc *wilc, u32 addr, u32 *data)
 		cmd.address = WILC_SDIO_FBR_DATA_REG;
 		cmd.block_mode = 0;
 		cmd.increment = 1;
-		cmd.count = sizeof(u32);
+		cmd.count = 4;
 		cmd.buffer = (u8 *)data;
-		cmd.use_global_buf = true;
 
 		cmd.block_size = sdio_priv->block_size;
 		ret = wilc_sdio_cmd53(wilc, &cmd);
@@ -631,7 +603,6 @@ static int wilc_sdio_read(struct wilc *wilc, u32 addr, u8 *buf, u32 size)
 	nblk = size / block_size;
 	nleft = size % block_size;
 
-	cmd.use_global_buf = false;
 	if (nblk > 0) {
 		cmd.block_mode = 1;
 		cmd.increment = 1;
